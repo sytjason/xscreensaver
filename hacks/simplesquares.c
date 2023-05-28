e26d097f9ef213a1dda3276f3204863568c3293f
sytjason <jasont1020508@gmail.com>
2023-05-28 19:16:12 +0800

    update simple

--- hacks/simplesquares.c
+++ hacks/simplesquares.c
@@ -9,9 +9,10 @@ int main ()
 {
   Display *dpy;
   Window root;
+  XWindowAttributes wa;
   GC g;
-  Screen s;
-  int snum, width, height;
+  int width, height;
+  XPoint center;
 
 
   /* open the display (connect to the X server)
@@ -19,8 +20,8 @@ int main ()
    */
   dpy = XOpenDisplay (getenv ("DISPLAY"));
   snum = DefaultScreen(dpy);
-  width = DisplayWidth (dpy, snum);
-  height = DisplayHeight (dpy, snum);
+
+  center.x = width/2, center.y = height/2;
 
   printf("Default screen num: %d\n", snum);
   printf("Dimensions: %d x %d\n", width, height);
@@ -28,24 +29,43 @@ int main ()
   /* get the root window */
   root = DefaultRootWindow (dpy);
 
+  /* get window attributes */
+  XGetWindowAttributes(dpy, root, &wa);
+
+  /* set screen dimensions */
+  width = wa.width;
+  height = wa.height;
+
   /* create a GC for drawing in the window */
   g = XCreateGC (dpy, root, 0, NULL);
 
   /* set foreground color */
-  // XSetForeground (dpy, g, WhitePixelOfScreen(DefaultScreenOfDisplay(dpy)) );
   XSetForeground (dpy, g, WhitePixelOfScreen(DefaultScreenOfDisplay(dpy)) );
-
 
   /* draw something */
   while (1)
   {
+    XPoint points[4];
+    short psize = 30;
+    points[0].x = -psize, points[0].y = psize;
+    points[1].x = psize, points[1].y = psize;
+    points[2].x = psize, points[2].y = -psize;
+    points[3].x = -psize, points[3].y = -psize;
+    for (int i = 0; i < 4; i++) {
+      points[i].x += center.x;
+      points[i].y += center.y;
+    }
+
     /* draw a little square */
-    XFillRectangle (dpy, root, g, random()%width, random()%height, 10, 10);
+    // XFillRectangle (dpy, root, g, random()%width, random()%height, 10, 10);
 
     /* draw a polygon */
+    XFillPolygon (dpy, root, g, points, 4, Convex, CoordModeOrigin);
 
-
-
+    /* draw a point */
+    // XDrawPoint(dpy, root, g, random()%width, random()%height);
+    
+    /* draw points */
 
     /* once in a while, clear all */
     if( random()%500<1 )
